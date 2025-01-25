bf30430c6235cbeacaa7663d4625a04a8bb469c7
Tegveer Singh <tegveersingh9271@gmail.com>
2025-01-25 03:58:43 -0500

    Added Initial Calendar display

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { first } from 'rxjs';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
@@ -13,7 +14,7 @@ export class CalendarComponent {
   selected_date: Date = new Date();
   selected_month: number = 0;
   selected_year: number = 0;
-  days_nums = [0, 1, 2, 3, 4, 5, 6];
+  month_strings = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   days_strings = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
   ngOnInit() {
@@ -21,13 +22,16 @@ export class CalendarComponent {
       this.selected_date.getMonth(),
       this.selected_date.getFullYear()
     );
+
+    this.selected_month = this.selected_date.getMonth();
   }
 
   // Get a list of calendar dates given the month and year
   refreshDate(month: number, year: number) {
+    
     // Get first date to be displayed in the calendar
     let temp_date = new Date(year, month, 1);
-    
+
     while (temp_date.getDay() !== 0) {
       temp_date.setDate(temp_date.getDate() - 1);
     }
@@ -38,7 +42,7 @@ export class CalendarComponent {
     for (let i = 0; i < 42; i++) {
       current_week.push(new Date(temp_date));
       if (i % 7 === 6) {
-        this.calendar_dates_lists.push(current_week)
+        this.calendar_dates_lists.push(current_week);
         current_week = [];
       }
       temp_date.setDate(temp_date.getDate() + 1);
@@ -46,4 +50,5 @@ export class CalendarComponent {
 
     console.log("Calendar Dates Lists: ", this.calendar_dates_lists);
   }
+
 }
