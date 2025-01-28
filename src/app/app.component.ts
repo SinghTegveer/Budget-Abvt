7732451e35a2c0b94f492adc8965910e9d64d977
Tegveer Singh <tegveersingh9271@gmail.com>
2025-01-27 21:42:06 -0500

    Scrolling Animations layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalendarComponent } from './calendar/calendar.component';
 import { ScrollItemComponent } from './scroll-item/scroll-item.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CalendarComponent, ScrollItemComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'budget-buddy';
-  accountsScrollTitle: string = "Accounts"
+  accountsScrollTitle: string = 'Accounts';
+  transactionsScrollTitle: string = 'Transactions';
+  budgetCalendarScrollTitle: string = 'Budgetting Calendar';
 }
--- src/app/scroll-item/scroll-item.component.ts
+++ src/app/scroll-item/scroll-item.component.ts
@@ -2,6 +2,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarComponent } from '../calendar/calendar.component';
 
 @Component({
   selector: 'app-scroll-item',
@@ -11,24 +12,36 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   styleUrl: './scroll-item.component.css',
   animations: [
     trigger('slideDown', [
-      state('A',
-        style({
-          backgroundColor: 'yellow'
+      state('A', style({
+          height: 0,
+          opacity: 0,
+          padding: 0,
         })
       ),
-      state(
-        'B',
-        style({
-          backgroundColor: 'blue'
+      state('B', style({
+          height: "*",
+          opacity: 1,
+          padding: '3%'
         })
       ),
-      transition('A <=> B', [animate('5000ms')]),
+      transition('A <=> B', [animate('500ms')]),
     ]),
   ],
 })
 export class ScrollItemComponent {
   @Input() scrollItemTitle: any;
+  displayComponent: any;
   scrollItemExpand: Boolean = false;
+
+  ngOnInit() {
+    if (this.scrollItemTitle === 'Accounts') {
+      this.displayComponent = CalendarComponent;
+    } else if (this.scrollItemTitle === 'Transactions') {
+      this.displayComponent = CalendarComponent;
+    } else if (this.scrollItemTitle == 'Budgetting Calendar') {
+      this.displayComponent = CalendarComponent;
+    }
+  }
 
   updateScrollItemExpand() {
     this.scrollItemExpand = !this.scrollItemExpand;
